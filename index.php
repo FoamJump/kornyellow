11d3b7b1ea5c9e70f9c2cd96d1fc6377162ce391
kornyellow <kornkubzaza@gmail.com>
2022-06-17 23:05:37 +0700

    Update

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+use libraries\methods\utils\Network;
+
 // Make errors visible
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -8,59 +10,35 @@ error_reporting(E_ALL);
 // Set timezone
 date_default_timezone_set('Asia/Bangkok');
 
-// Essential functions
-function getAbsolutePath(string $path): string {
-	$path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
-	$parts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
-	$absolutes = array();
-	foreach ($parts as $part) {
-		if ($part == '.')
-			continue;
-
-		if ($part == '..')
-			array_pop($absolutes);
-		else
-			$absolutes[] = $part;
-	}
-	return implode(DIRECTORY_SEPARATOR, $absolutes);
-}
-function redirectPage($url, $delay = 0) {
-	echo '<meta http-equiv="refresh" content="'.$delay.' url='.$url.'" />';
-}
-
 // Find requested path
-$currentDomainURL = 'http://'.$_SERVER['HTTP_HOST'];
-if ($_SERVER['HTTPS'] ?? false)
-	$currentDomainURL = 'https://'.$_SERVER['HTTP_HOST'];
-$requestURI = $_SERVER['REQUEST_URI'];
+$currentDomainURL = Network::getCurrentDomainURL();
 
-$requestPath = ($requestURI  == '/') ? '' : substr($requestURI, 1);
-$absolutePath = getAbsolutePath($requestPath);
-if ($requestPath != $absolutePath) {
-	redirectPage($currentDomainURL.'/'.$absolutePath);
-	exit;
+$requestPath  = Network::getRequestPath();
+$absolutePath = Network::getAbsolutePath($requestPath);
+if($requestPath != $absolutePath) {
+	Network::redirectPage($currentDomainURL.'/'.$absolutePath);
 }
 
-// Cannot access direct index.php
-if (str_ends_with($absolutePath, 'index.php')) {
+// Preventing user from accessing direct index.php
+if(str_ends_with($absolutePath, 'index.php')) {
 	$absolutePath = substr($absolutePath, 0, -9);
-	redirectPage($currentDomainURL.'/'.$absolutePath);
-	exit;
+	Network::redirectPage($currentDomainURL.'/'.$absolutePath);
 }
 
-// Construct page
-$requestFile = $_SERVER['DOCUMENT_ROOT'].'/contents/';
+// Find a requested file
+$requestFile = Network::getDocumentRoot().'/contents/';
 
-if ($absolutePath == '')
+if($absolutePath == '')
 	$requestFile .= 'home.php';
 else if(!file_exists($requestFile.$absolutePath.'.php'))
 	$requestFile .= $absolutePath.'/index.php';
 else
 	$requestFile .= $absolutePath.'.php';
 
+// Construct an entire page
 include('templates/header.php');
 
-if (file_exists($requestFile))
+if(file_exists($requestFile))
 	include($requestFile);
 else
 	include('templates/errors/404.php');
--- libraries/methods/utils/Network.php
+++ libraries/methods/utils/Network.php
@@ -1,29 +1,46 @@
 <?php
+
 namespace libraries\methods\utils;
 
+use JetBrains\PhpStorm\NoReturn;
+
 class Network {
+	private static string $header;
+	private static string $description;
+	private static string $abstract;
+	
+	public static function getHeader(): string {
+		return self::$header;
+	}
+	public static function getDescription(): string {
+		return self::$description;
+	}
+	public static function getAbstract(): string {
+		return self::$abstract;
+	}
+	
 	public static function getRequestPath(): string {
 		$requestURI = $_SERVER['REQUEST_URI'];
-		if ($requestURI == "/")
-			return "";
-
+		if($requestURI == '/')
+			return '';
+		
 		return substr($requestURI, 1);
 	}
 	public static function getRemoteIP(): string {
 		$clientIP = $_SERVER['HTTP_CLIENT_IP'];
-		if (filter_var($clientIP ?? "", FILTER_VALIDATE_IP))
+		if(filter_var($clientIP ?? '', FILTER_VALIDATE_IP))
 			return $clientIP;
-
+		
 		return $_SERVER['REMOTE_ADDR'];
 	}
 	public static function getCurrentDomain(): string {
 		return $_SERVER['HTTP_HOST'];
 	}
 	public static function getCurrentDomainURL(): string {
-		if (self::isHTTPS())
-			return "https://".self::getHost();
-
-		return "http://".self::getHost();
+		if(self::isHTTPS())
+			return 'https://'.self::getHost();
+		
+		return 'http://'.self::getHost();
 	}
 	public static function getDocumentRoot(): string {
 		return $_SERVER['DOCUMENT_ROOT'];
@@ -31,36 +48,39 @@ class Network {
 	public static function getHost(): string {
 		return $_SERVER['HTTP_HOST'];
 	}
+	
 	public static function getAbsolutePath(string $path): string {
-		$path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
-		$parts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
-		$absolutes = array();
-		foreach ($parts as $part) {
-			if ($part == '.')
+		$path      = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $path);
+		$parts     = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
+		$absolutes = [];
+		foreach($parts as $part) {
+			if($part == '.')
 				continue;
-
-			if ($part == '..')
+			
+			if($part == '..')
 				array_pop($absolutes);
 			else
 				$absolutes[] = $part;
 		}
+		
 		return implode(DIRECTORY_SEPARATOR, $absolutes);
 	}
-
+	
 	public static function isLocalHost(): bool {
-		if (self::getRemoteIP() == '127.0.0.1')
+		if(self::getRemoteIP() == '127.0.0.1')
 			return true;
-
+		
 		return false;
 	}
 	public static function isHTTPS(): bool {
-		if ($_SERVER['HTTPS'] ?? false)
+		if($_SERVER['HTTPS'] ?? false)
 			return true;
-
+		
 		return false;
 	}
-
-	public static function redirectPage($url, $delay = 0) {
-		echo '<meta http-equiv="refresh" content="'.$delay.' url='.$url.'" />';
+	
+	#[NoReturn] public static function redirectPage($url, $delay = 0): void {
+		echo '<meta http-equiv="refresh" content="'.$delay.'" url = "'.$url.'" />';
+		exit;
 	}
 }
